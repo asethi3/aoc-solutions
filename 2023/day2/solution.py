930e40ad51b432e506f28f88dcbeea579a4cd116
abhinav-vt <abhinav.sethi@tulip.co>
2023-12-02 02:30:23 -0500

    clean up

--- 2023/day2/solution.py
+++ 2023/day2/solution.py
@@ -1,27 +1,24 @@
-from collections import defaultdict
-from functools import reduce
-
-limits = {'red': 12, 'green': 13, 'blue': 14}
+colors = {'red': 12, 'green': 13, 'blue': 14}
 
 def is_draw_ok(s):
   count = int(s.split()[0])
-  return not any([c in s and count > limits[c] for c in limits])
+  return not any([c in s and count > colors[c] for c in colors])
 
-def part_1(lines):
+def part_1(games):
   count = 0
-  for i, line in enumerate(lines):
+  for i, line in enumerate(games):
     sets = line.split(':')[1].split(';')
     if all([all([is_draw_ok(s.strip()) for s in set.split(',')]) for set in sets]):
       count += i+1
   return count
 
 def get_counts(set):
-    counts = {s.split()[1]: int(s.split()[0]) for s in set.split(',') if s.split()[1] in limits}
+    counts = {s.split()[1]: int(s.split()[0]) for s in set.split(',') if s.split()[1] in colors}
     return counts.get('red', 0), counts.get('green', 0), counts.get('blue', 0)
 
-def part_2(lines):
+def part_2(games):
   power_sum = 0
-  for line in lines:
+  for line in games:
     sets = line.split(':')[1].split(';')
     max_r, max_b, max_g = 0, 0, 0
     for set in sets:
@@ -31,6 +28,6 @@ def part_2(lines):
   return power_sum
 
 with open('input.txt', 'r') as f:
-  lines = f.readlines()
-  print(part_1(lines))
-  print(part_2(lines))
+  data = f.read().strip().split('\n')
+  print(part_1(data))
+  print(part_2(data))
